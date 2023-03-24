91ce8d528cf0f3474980ff9a59ef499e14ce4cf0
Dongdong Kong <kongdd@users.noreply.github.com>
2023-03-24 10:11:49 +0800

    format anusplin

--- R/anusplin.R
+++ R/anusplin.R
@@ -115,92 +115,98 @@
 #' }
 #' @importFrom glue glue
 #' @importFrom stringr str_extract
-#' @importFrom magrittr %>% %<>% 
+#' @importFrom magrittr %>% %<>%
 #' @importFrom data.table as.data.table
-#'
-#' @examples 
+#' 
+#' @examples
 #' data(TempBrazil)
-#' colnames(TempBrazil) <- c('lon', 'lat', 'temp')
-#' anusplin_params(TempBrazil, 'TempBrazil', c(70, 140, 15, 55), 'dem.txt', alt = NULL)
-anusplin_params <- function(dat,
-                            basename,
-                            range,
-                            file.alt,
-                            unit = 0,
-                            res = 0.25,
-                            width = 7,
-                            alt = 'cov',
-                            lim.lon = 'auto',
-                            lim.lat = 'auto',
-                            lim.alt = 'auto',
-                            cvt.lon = c(0, 1),
-                            cvt.lat = c(0, 1),
-                            cvt.alt = c(1, 1),
-                            cvt.coef = 1000,
-                            trans.dep = 0,
-                            order = 3,
-                            err.wgt = 0,
-                            optimize = 1,
-                            smooth = 1,
-                            type.mask = 0,
-                            file.mask = NULL,
-                            type.alt = 2,
-                            type.grd = 2,
-                            missing = -9999,
-                            err.cov = 2,
-                            grid.pos = 1,
-                            essential = T) {
+#' colnames(TempBrazil) <- c("lon", "lat", "temp")
+#' anusplin_params(TempBrazil, "TempBrazil", c(70, 140, 15, 55), "dem.txt", alt = NULL)
+anusplin_params <- function(
+    dat, basename, range, file.alt,
+    unit = 0,
+    res = 0.25,
+    width = 7,
+    alt = "cov",
+    lim.lon = "auto", lim.lat = "auto", lim.alt = "auto",
+    cvt.lon = c(0, 1), cvt.lat = c(0, 1), cvt.alt = c(1, 1),
+    cvt.coef = 1000,
+    trans.dep = 0,
+    order = 3,
+    err.wgt = 0,
+    optimize = 1,
+    smooth = 1,
+    type.mask = 0,
+    file.mask = NULL,
+    type.alt = 2,
+    type.grd = 2,
+    missing = -9999,
+    err.cov = 2,
+    grid.pos = 1,
+    essential = T) {
   # check arguments validation
   if (nchar(missing) > width) {
-    stop('Missing values should not be more than the width')
+    stop("Missing values should not be more than the width")
   }
   if (type.alt != 0) {
-    if (!is.character(file.alt))
-      stop('1')
+    if (!is.character(file.alt)) {
+      stop("1")
+    }
   } else {
-    if (!is.numeric(file.alt))
-      stop('2')
+    if (!is.numeric(file.alt)) {
+      stop("2")
+    }
   }
-  
+
   dat %<>% as.data.table()
   names <- colnames(dat)
-  
+
   if (is.null(alt)) {
-    if ('alt' %in% names) {
+    if ("alt" %in% names) {
       dat$alt <- NULL
-      names <- names[names != 'alt']
+      names <- names[names != "alt"]
     }
     cvt.alt <- NULL
-    
   } else {
-    cvt.alt <- if (lim.alt == 'auto') {
-      dat$alt %>% range %>% c(cvt.alt) %>% paste(collapse = ' ')
+    cvt.alt <- if (lim.alt == "auto") {
+      dat$alt %>%
+        range() %>%
+        c(cvt.alt) %>%
+        paste(collapse = " ")
     } else {
-      paste(lim.alt, cvt.alt, collapse = ' ')
+      paste(lim.alt, cvt.alt, collapse = " ")
     }
   }
-  
+
   # n.sur <- length(names) - 2
-  
+
   ind.spl <- 2
   ind.cov <- sur.spl <- sur.cov <- 0
-  
-  cvt.lon <- if (lim.lon == 'auto') {
-    dat$lon %>% range %>% c(cvt.lon) %>% paste(collapse = ' ')
+
+  cvt.lon <- if (lim.lon == "auto") {
+    dat$lon %>%
+      range() %>%
+      c(cvt.lon) %>%
+      paste(collapse = " ")
   } else {
-    paste(lim.lon, cvt.lon, collapse = ' ')
+    paste(lim.lon, cvt.lon, collapse = " ")
   }
-  
-  cvt.lat <- if (lim.lat == 'auto') {
-    dat$lat %>% range %>% c(cvt.lat) %>% paste(collapse = ' ')
+
+  cvt.lat <- if (lim.lat == "auto") {
+    dat$lat %>%
+      range() %>%
+      c(cvt.lat) %>%
+      paste(collapse = " ")
   } else {
-    paste(lim.lat, cvt.lat, collapse = ' ')
+    paste(lim.lat, cvt.lat, collapse = " ")
   }
-  
-  if ('site' %in% names) {
-    pos.site <- which('site' == names)
-    chr.site <- dat$site %>% nchar %>% max
-    fmt.site <- glue('%{chr.site}i')
+
+  if ("site" %in% names) {
+    pos.site <- which("site" == names)
+    chr.site <- dat$site %>%
+      nchar() %>%
+      max()
+    fmt.site <- glue("%{chr.site}i")
     dat$site %<>% sprintf(fmt = fmt.site)
     # n.sur <- n.sur - 1
   } else {
@@ -208,112 +214,103 @@ anusplin_params <- function(dat,
     pos.site <- -1
     fmt.site <- NULL
   }
-  
-  pos.coord = which(names %in% c('lon', 'lat'))
+
+  pos.coord <- which(names %in% c("lon", "lat"))
   if (length(pos.coord) != 2) {
-    stop('Check whether lon lat in colnames of dat')
+    stop("Check whether lon lat in colnames of dat")
   }
   dat[, pos.coord] <-
-    dat[, lapply(.SD, sprintf, fmt = glue('%{width}.2f')), .SDcols = pos.coord]
-  
-  if (!is.null(alt) & 'alt' %in% names) {
-    dat$alt %<>% sprintf(fmt = glue('%{width}.1f'))
+    dat[, lapply(.SD, sprintf, fmt = glue("%{width}.2f")), .SDcols = pos.coord]
+
+  if (!is.null(alt) & "alt" %in% names) {
+    dat$alt %<>% sprintf(fmt = glue("%{width}.1f"))
     # n.sur <- n.sur - 1
-    if (alt == 'cov') {
+    if (alt == "cov") {
       ind.cov <- ind.cov + 1
-    } else if (alt == 'spl') {
+    } else if (alt == "spl") {
       ind.spl <- ind.spl + 1
     }
   }
-  
-  
-  pos.var <- which(!names %in% c('site', 'lon', 'lat', 'alt'))
+
+  pos.var <- which(!names %in% c("site", "lon", "lat", "alt"))
   n.sur <- length(pos.var)
   dat[, pos.var] <-
-    dat[, lapply(.SD, sprintf, fmt = glue('%{width}.2f')), .SDcols = pos.var]
-  
-  file.sur <- glue('{basename}.sur')
-  file.cov <- glue('{basename}.cov')
+    dat[, lapply(.SD, sprintf, fmt = glue("%{width}.2f")), .SDcols = pos.var]
+
+  file.sur <- glue("{basename}.sur")
+  file.cov <- glue("{basename}.cov")
   if (essential) {
-    output <- c('', '', file.sur, '', file.cov)
+    output <- c("", "", file.sur, "", file.cov)
   } else {
     output <- c(
-      glue('{basename}.res'),
-      glue('{basename}.opt'),
+      glue("{basename}.res"),
+      glue("{basename}.opt"),
       file.sur,
-      glue('{basename}.lis'),
+      glue("{basename}.lis"),
       file.cov
     )
   }
-  
-  rle <- str_extract(dat[1], "(?<=\\.)[0-9]+") %>% nchar %>% rle
-  rle$lengths[which(rle$lengths == 1)] <- ''
-  rle$values[which(!is.na(rle$value))] %<>% paste0('f', width, '.', .)
+
+  rle <- str_extract(dat[1], "(?<=\\.)[0-9]+") %>%
+    nchar() %>%
+    rle()
+  rle$lengths[which(rle$lengths == 1)] <- ""
+  rle$values[which(!is.na(rle$value))] %<>% paste0("f", width, ".", .)
   rle$values[which(is.na(rle$values))] <-
-    glue('a{chr.site}')
-  
-  fmt <- paste0(rle[['lengths']], rle[['values']]) %>%
-    paste(collapse = ',') %>%
-    paste0('(', ., ')')
-  
+    glue("a{chr.site}")
+
+  fmt <- paste0(rle[["lengths"]], rle[["values"]]) %>%
+    paste(collapse = ",") %>%
+    paste0("(", ., ")")
+
   splina <- c(
     basename,
     unit,
-    ind.spl,
-    ind.cov,
-    sur.spl,
-    sur.cov,
-    cvt.lon,
-    cvt.lat,
-    cvt.alt,
-    cvt.coef,
-    paste(trans.dep, collapse = ' '),
+    ind.spl, ind.cov,
+    sur.spl, sur.cov,
+    cvt.lon, cvt.lat, cvt.alt, cvt.coef,
+    paste(trans.dep, collapse = " "),
     order,
     n.sur,
     err.wgt,
     optimize,
     smooth,
-    glue('{basename}.dat'),
+    glue("{basename}.dat"),
     round_any(nrow(dat), 5, f = ceiling),
     chr.site,
     fmt,
     output,
-    '',
-    ''
+    "",
+    ""
   )
-  
+
   lapgrd <- c(
     file.sur,
     0,
     1,
     file.cov,
     err.cov,
-    '',
+    "",
     grid.pos,
     pos.coord[1],
-    glue('{range[1]} {range[2]} {res}'),
+    glue("{range[1]} {range[2]} {res}"),
     pos.coord[2],
-    glue('{range[3]} {range[4]} {res}'),
+    glue("{range[3]} {range[4]} {res}"),
     ifelse(type.mask, c(type.mask, file.mask), type.mask),
     type.alt,
     file.alt,
     type.grd,
     missing,
-    glue('{names[pos.var]}.grd'),
-    glue('({n.sur + 2}f{width}.2)'),
+    glue("{names[pos.var]}.grd"),
+    glue("({n.sur + 2}f{width}.2)"),
     type.grd,
     missing,
-    glue('cov_{names[pos.var]}.grd'),
-    glue('({n.sur + 2}f{width}.2)'),
-    '',
-    ''
+    glue("cov_{names[pos.var]}.grd"),
+    glue("({n.sur + 2}f{width}.2)"),
+    "",
+    ""
   )
-  
-  return(list(
-    data = dat,
-    splina = splina,
-    lapgrd = lapgrd
-  ))
+  list(data = dat, splina = splina, lapgrd = lapgrd)
 }
 
 
@@ -336,68 +333,71 @@ anusplin_params <- function(dat,
 #' @importFrom stringr str_match_all
 #' @importFrom stats na.omit
 #'
-anusplin_write <-
-  function(dat,
-           opt_splina,
-           opt_lapgrd,
-           file_path,
-           exe,
-           na.width = 'auto',
-           names = c('splina.txt', 'lapgrd.txt'),
-           cmd = NULL) {
-    width <- dat[, lapply(.SD, nchar)] %>% na.omit %>% unique %>% nrow
-    if (width != 1)
-      stop('Data column width is not fixed.')
-    
-    if (na.width == 'auto') {
-      na.col <- which(sapply(a$data, anyNA)) %>% unname()
-      if (length(na.col) > 0) {
-        revid <- opt_splina[length(opt_splina) - 7] %>%
-          {
-            str_match_all(., '([0-9]+)?[a-z]([0-9]+)')[[1]]
-          } %>%
-          apply(1, \(x) rep(as.numeric(x[3]), ifelse(is.na(x[2]), 1, as.numeric(x[2])))) %>%
-          unlist
-        na.width <- revid[na.col] %>% unique
-        if (length(na.width) != 1) {
-          stop(
-            glue(
-              'The widths of columns containing NA must be unique, but widths are {na.width}'
-            )
-          )
-        }
-      } else
-        na.width <- NULL
-    }
-    
-    file <- opt_splina[length(opt_splina) - 10]
-    write.table(
-      dat,
-      glue("{file_path}/{file}"),
-      sep = "",
-      col.names = F,
-      row.names = F,
-      quote = F,
-      na = strrep(" ", na.width)
-    )
-    
-    splina <- unlist(opt_splina, use.names = F)
-    fileConn <- glue("{file_path}/{names[1]}") %>% file()
-    writeLines(splina, fileConn)
-    close(fileConn)
-    
-    lapgrd <- unlist(opt_lapgrd, use.names = F)
-    fileConn <- glue("{file_path}/{names[2]}") %>% file()
-    writeLines(lapgrd, fileConn)
-    close(fileConn)
-    
-    if ((length(exe) == 2) && !is.null(cmd)) {
-      cmd <- c(
-        glue('{exe[1]}<{names[1]}>splina.log'),
-        glue('{exe[2]}<{names[2]}>lapgrd.log')
-      )
-      fileConn <- glue("{file_path}/{cmd}") %>% file()
-      writeLines(lapgrd, fileConn)
-      close(fileConn)
+#' @export
+anusplin_write <- function(dat,
+                           opt_splina,
+                           opt_lapgrd,
+                           file_path,
+                           exe,
+                           na.width = "auto",
+                           names = c("splina.txt", "lapgrd.txt"),
+                           cmd = NULL) {
+  width <- dat[, lapply(.SD, nchar)] %>%
+    na.omit() %>%
+    unique() %>%
+    nrow()
+  if (width != 1) {
+    stop("Data column width is not fixed.")
+  }
+
+  if (na.width == "auto") {
+    na.col <- which(sapply(a$data, anyNA)) %>% unname()
+    if (length(na.col) > 0) {
+      revid <- opt_splina[length(opt_splina) - 7] %>%
+        {
+          str_match_all(., "([0-9]+)?[a-z]([0-9]+)")[[1]]
+        } %>%
+        apply(1, \(x) rep(as.numeric(x[3]), ifelse(is.na(x[2]), 1, as.numeric(x[2])))) %>%
+        unlist()
+      na.width <- revid[na.col] %>% unique()
+      if (length(na.width) != 1) {
+        stop(
+          glue("The widths of columns containing NA must be unique, but widths are {na.width}")
+        )
+      }
+    } else {
+      na.width <- NULL
     }
   }
+
+  file <- opt_splina[length(opt_splina) - 10]
+  write.table(
+    dat,
+    glue("{file_path}/{file}"),
+    sep = "",
+    col.names = F,
+    row.names = F,
+    quote = F,
+    na = strrep(" ", na.width)
+  )
+
+  splina <- unlist(opt_splina, use.names = F)
+  fileConn <- glue("{file_path}/{names[1]}") %>% file()
+  writeLines(splina, fileConn)
+  close(fileConn)
+
+  lapgrd <- unlist(opt_lapgrd, use.names = F)
+  fileConn <- glue("{file_path}/{names[2]}") %>% file()
+  writeLines(lapgrd, fileConn)
+  close(fileConn)
+
+  if ((length(exe) == 2) && !is.null(cmd)) {
+    cmd <- c(
+      glue("{exe[1]}<{names[1]}>splina.log"),
+      glue("{exe[2]}<{names[2]}>lapgrd.log")
+    )
+    fileConn <- glue("{file_path}/{cmd}") %>% file()
+    writeLines(lapgrd, fileConn)
+    close(fileConn)
+  }
+}
