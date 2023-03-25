233971bd024d361f637832c3e3b8c89b33f8aa63
songhy <7800556@gmail.com>
2023-03-25 14:07:57 +0800

    anusplin: Increase robustness & add example

--- R/examples/ex-anusplin.R
+++ R/examples/ex-anusplin.R
@@ -1,9 +1,19 @@
-data(TempBrazil)
-colnames(TempBrazil) <- c("lon", "lat", "temp")
-df = TempBrazil
+# data(TempBrazil)
+# colnames(TempBrazil) <- c("lon", "lat", "temp")
+# df = TempBrazil
+# 
+# # range <- c(70, 140, 15, 55)
+# range <- c(-78, -34, -36, 5)
+# p = anusplin_params(df, "TempBrazil", "dem.txt", range, alt = NULL)
+# str(p)
 
-# range <- c(70, 140, 15, 55)
-range <- c(-78, -34, -36, 5)
-p = anusplin_params(df, "TempBrazil", "dem.txt", range, alt = NULL)
-str(p)
+library(data.table)
+library(magrittr)
 
+dat <- fread('inst/extdata/anusplin_demo/RH_01-01.dat') %>% 
+  `colnames<-`(c('site', 'lon', 'lat', 'alt', 'RH'))
+
+range <- c(69.625, 140.375, 14.625, 55.375)
+
+params <- anusplin_params(dat, 'RH', range, 'inst/extdata/anusplin_demo/china_dem_025deg.txt')
+anusplin_write('R/examples/anusplin_output', params, is.run = T)
